98647123044ed7d5a791befb06e6aa58253b3da6
Luz42 <a.evangelista93@gmail.com>
2022-09-21 10:20:44 +0200

    STEP 5
    con un v-for gli elementi (email) vengono stampati in HTML
    
    BONUS
    viene aggiunta una condizione per visualizzare i dati una volta caricati
    
    viene aggiunta un'altra condizione per visualizzare e comunicare errori

--- myScript.js
+++ myScript.js
@@ -4,6 +4,7 @@ console.log('JS-OK');
      {
          el: '#root',
          data: {
+            loading: true,
             emails: []
          },
 
@@ -11,20 +12,29 @@ console.log('JS-OK');
  
 created(){
  //let elementHTML = document.getElementById('root')
-
-for(let i = 0; i < 10; i++){
+const NUM_EMAILS = 10;
+ let count = 0
+for(let i = 0; i < NUM_EMAILS; i++){
     axios
         .get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response) => {
-
+            console.log(response)
             const { data, status } = response;
+            count ++
             //elementHTML.innerHTML += `<li>${result}</li>`
             if (status === 200){
-            this.emails.push(data.response)
-            console.log(this.emails)
+                this.emails.push(data.response)
+                //console.log(this.emails)
+                //this.loading = this.emails.length < 10
             }
-        }
-    )
+            if (count === NUM_EMAILS) {
+                this.loading = false
+            }
+        })
+        // .catch() intercetta eventuali errori
+        .catch((error) => {
+            console.log('ERRORE', error)
+        })
 };
 },
 });
